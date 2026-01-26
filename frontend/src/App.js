b62c2e5498f7084a8fafe2cefe437f1412b006e8
root <root@ip-172-31-19-236.ec2.internal>
2026-01-26 09:56:55 +0000

    Added React frontend application

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+	  return (
+		      <div style={{ textAlign: "center", marginTop: "60px" }}>
+		        <h1> Three Tier Application</h1>
+		        <p>Frontend: React JS</p>
+		        <p>Backend: Node.js</p>
+		        <p>Database: MongoDB</p>
+		      </div>
+		    );
+}
+
+export default App;
+
