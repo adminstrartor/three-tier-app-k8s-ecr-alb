6d9f5d45782ee284bfe3a3e16018edb3c38ef3ea
adminstrartor <159612643+adminstrartor@users.noreply.github.com>
2026-01-28 20:51:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,28 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-// MongoDB connection (container name, not localhost)
-// const MONGO_URL = "mongodb://mongodb-container:27017/testdb";
-//
-// app.use(cors());
-// app.use(express.json());
-//
-// // Connect to MongoDB
-// mongoose
-//   .connect(MONGO_URL)
-//     .then(() => console.log("MongoDB connected successfully"))
-//       .catch((err) => console.error("MongoDB connection error:", err));
-//
-//       app.get("/api/health", (req, res) => {
-//         res.json({
-//             status: "UP",
-//                 message: "Backend is running and connected to MongoDB",
-//                   });
-//                   });
-//
-//                   app.listen(PORT, () => {
-//                     console.log(`Backend server running on port ${PORT}`);
-//                     });
+// MongoDB URL from env
+const MONGO_URL =
+  process.env.MONGO_URL || "mongodb://mongodb-service:27017/three-tier-db";
+
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "UP",
+    message: "Backend is running successfully",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server running on port ${PORT}`);
+});
